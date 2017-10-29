8bd76eb9528135444faca97881a2c76b4e0779c9
Charlie441324219 <dian.li@ttu.edu>
2017-10-29 15:01:46 -0500

    implemented confguration

--- src/edu/ttu/spm/DatabaseTestingRobot/Configuration.java
+++ src/edu/ttu/spm/DatabaseTestingRobot/Configuration.java
@@ -1,4 +1,31 @@
 package edu.ttu.spm.DatabaseTestingRobot;
 
 public class Configuration {
+
+    private String baseURL;
+    private int requestTime;
+
+    public Configuration() {
+    }
+
+    public Configuration(String baseURL, int requestTime) {
+        this.baseURL = baseURL;
+        this.requestTime = requestTime;
+    }
+
+    public void setBaseURL(String baseURL) {
+        this.baseURL = baseURL;
+    }
+
+    public void setRequestTime(int requestTime) {
+        this.requestTime = requestTime;
+    }
+
+    public String getBaseURL() {
+        return baseURL;
+    }
+
+    public int getRequestTime() {
+        return requestTime;
+    }
 }
